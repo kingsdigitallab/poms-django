60c338d0b780b5a879f6a9524670aedabea9f6b9
Elliott Hall <elliott.hall@kcl.ac.uk>
2018-04-17 11:18:24 +0100

    Added templates, main admin

---
+++ pomsapp/databrowse_load.py
@@ -0,0 +1,27 @@
+# DATABROWSE setup
+from django.contrib import databrowse
+from pomsapp.models import *
+
+
+# authority lists
+
+auth_lists = [Role, Gender, Floruit, Chartertype, TitleType, Relationshiptype, Occupationtype, Exemptiontype,
+				Nominalrendertype, Proanimagenerictypes, Renderdate, Sicutclausetype, Tenendasclauseoptions, 
+				Transactiontype, LegalPertinents, Returns_military, Returns_renders, CommonBurdens, Language]
+
+func = lambda x : databrowse.site.register(x)
+map(func, auth_lists)
+
+
+# now the main models
+
+databrowse.site.register(Person)
+databrowse.site.register(Charter)
+# databrowse.site.register(Possession)
+# databrowse.site.register(Place)
+databrowse.site.register(FactTitle)
+databrowse.site.register(FactRelationship)
+# databrowse.site.register(FactOccupation)
+databrowse.site.register(FactPossession)
+# databrowse.site.register(FactReference)
+databrowse.site.register(FactTransaction)

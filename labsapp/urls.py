2a38dc3f254eb710b73cccf18a652e519de40c44
Elliott Hall <elliott.hall@kcl.ac.uk>
2018-04-23 12:43:54 +0100

    updated labsapp urls

--- labsapp/urls.py
+++ labsapp/urls.py
@@ -1,81 +1,127 @@
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.conf.urls import url
+from django.views.generic import TemplateView
+
+import labsapp.connectionscloud as connectionscloud
+import labsapp.highcharts_scatter as highcharts_scatter
+import labsapp.peoplelives as peoplelives
+import labsapp.popularitycloud as popularitycloud
+import labsapp.relationships as relationships
+import labsapp.witnessesRgraph as witnessesRgraph
+import labsapp.person2person as person2person
+import labsapp.bubbles as bubbles
+from labsapp.tree import tree1
+
+urlpatterns = [
+    # root url and main STATIC windows:
+    url(r'^$', TemplateView.as_view(template_name='labs/mainsite/indexLabs.html')),
+    url(r'^connectionscloud$', TemplateView.as_view(
+        template_name='labs/mainsite/connectionscloud.html', initkwargs={
+            'connectionscloud': True,
+        })),
+    url(
+        r'^peoplelives$', TemplateView.as_view(template_name='labs/mainsite/lives.html',
+                                               initkwargs={
+                                                   'peoplelives': True,
+                                               })),
+    url(r'^peopleimportance$',
+        TemplateView.as_view(template_name='labs/mainsite/importance.html',
+                             initkwargs={
+                                 'peopleimportance': True,
+                             })),
+    url(r'^witnesses$', TemplateView.as_view(template_name='labs/mainsite/witnesses.html',
+                                             initkwargs={
+                                                 'witnesses': True,
+                                             })),
+
+    # DYNAMIC stuff:
+
+    # CONNECTIONSCLOUD
+    # (r'^peoplenavigator$', 'labsapp.people_navigator.index' ),
+    url(r'^connectionscloud/go$', connectionscloud.index,
+        name='labsapp.connectionscloud.index'),
+    url(
+        r'^connectionscloud/ajax/getCommonConnections',
+        connectionscloud.getCommonConnections,
+        name='labsapp.connectionscloud.getCommonConnections'),
+
+    url(r'^connectionscloud/autocomplete/$',
+        connectionscloud.autocomplete_people,
+        name='autocomplete_people'),
+
+    # IMPORTANCE
+    url(r'^popularitycloud/go$',
+        popularitycloud.pop_cloud,
+        name='labsapp.popularitycloud.pop_cloud'),
+    # ajax: used here and also in Lives and Longevity Chart - so careful when modifying
+    url(r'^popularitycloud/ajax/personinfo',
+        popularitycloud.personinfo,
+        name='labsapp.popularitycloud.personinfo'),
+
+    # LIVES
+    url(r'^peoplelives/go$',
+        peoplelives.lives,
+        name='labsapp.peoplelives.lives'),
+    url(r'^peoplelives/ajax_lives1',
+        peoplelives.ajax_lives1,
+        'labsapp.peoplelives.ajax_lives1'),
+
+    # LONGEVITY CHART
+    url(r'^longevitychart/go$',
+        highcharts_scatter.scatter_peopleage,
+        name='labsapp.highcharts_scatter.scatter_peopleage'),
+    url(r'^longevitychart/ajax/iteminfo$',
+        highcharts_scatter.iteminfo,
+        name='labsapp.highcharts_scatter.iteminfo'),
+
+    # RELATIONSHIPS : SANKEY version
+    url(r'^relationships/go$',
+        relationships.index,
+        name='labsapp.relationships.index'),
+    url(r'^relationships/ajax/iteminfo',
+        relationships.iteminfo,
+        name='labsapp.relationships.iteminfo'),
+
+    # WITNESSES
+    url(r'^witnesses/go$',
+        witnessesRgraph.index,
+        name='labsapp.witnessesRgraph.index'),
+    url(r'^witnesses/go-halfsize$', witnessesRgraph.index,
+        name='labsapp.witnessesRgraph.index',
+        kwargs={'template': 'halfsize'}),
+    url(r'^witnesses/ajax/iteminfo', witnessesRgraph.iteminfo,
+        'labsapp.witnessesRgraph.iteminfo'),
+
+    # --------
+    # INTERNAL URLS - testing and backup
+    # --------
 
 
-urlpatterns = patterns('',
-	# root url and main STATIC windows:
-	(r'^$', direct_to_template, {'template': 'labs/mainsite/indexLabs.html'}),
-	(r'^connectionscloud$', direct_to_template, {'template': 'labs/mainsite/connectionscloud.html', 'extra_context': { 
-	    'connectionscloud': True, 
-	  }}),
-	(r'^peoplelives$', direct_to_template, {'template': 'labs/mainsite/lives.html', 'extra_context': { 
-	    'peoplelives': True, 
-	  }}),
-	(r'^peopleimportance$', direct_to_template, {'template': 'labs/mainsite/importance.html', 'extra_context': { 
-	    'peopleimportance': True, 
-	  }}),
-	(r'^witnesses$', direct_to_template, {'template': 'labs/mainsite/witnesses.html', 'extra_context': { 
-	    'witnesses': True, 
-	  }}),
+    # PERSON 2 PERSON  [dismissed]
+    url(r'^person2person/go$', person2person.index,
+        name='labsapp.person2person.index'),
+    url(r'^person2person/go-halfsize$', person2person.index,
+        name='labsapp.person2person.index', kwargs={'template': 'halfsize'}),
+    # (r'^person2person/ajax/iteminfo', 'labsapp.person2person.iteminfo' ),
 
-	# DYNAMIC stuff:
+    url(r'^bubbles$',
+        bubbles.index,
+        name='labsapp.bubbles.index'),
 
-	# CONNECTIONSCLOUD
-	# (r'^peoplenavigator$', 'labsapp.people_navigator.index' ),
-	(r'^connectionscloud/go$', 'labsapp.connectionscloud.index' ),
-	(r'^connectionscloud/ajax/getCommonConnections', 'labsapp.connectionscloud.getCommonConnections' ),
-	url(r'^connectionscloud/autocomplete/$', 'labsapp.connectionscloud.autocomplete_people', name='autocomplete_people'),
+    # highcharts
+    url(r'^scatter$',
+        highcharts_scatter.scatter,
+        name='labsapp.highcharts_scatter.scatter'),
+    url(r'^scatter_example$',
+        highcharts_scatter.scatter_example,
+        name='labsapp.highcharts_scatter.scatter_example'),
+    url(r'^scatterpeople$',
+        highcharts_scatter.scatter_peopleage,
+        'labsapp.highcharts_scatter.scatter_peopleage'),
 
+    # still in the making..
 
-	# IMPORTANCE
-	(r'^popularitycloud/go$', 'labsapp.popularitycloud.pop_cloud' ),
-	# ajax: used here and also in Lives and Longevity Chart - so careful when modifying 
-	(r'^popularitycloud/ajax/personinfo', 'labsapp.popularitycloud.personinfo' ),
-	
-		
-	# LIVES
-	(r'^peoplelives/go$', 'labsapp.peoplelives.lives' ),
-	(r'^peoplelives/ajax_lives1', 'labsapp.peoplelives.ajax_lives1' ),
+    url(r'^tree$',
+        tree1,
+        name='labsapp.tree.tree1'),
 
-	#LONGEVITY CHART
-	(r'^longevitychart/go$', 'labsapp.highcharts_scatter.scatter_peopleage' ),
-	(r'^longevitychart/ajax/iteminfo$', 'labsapp.highcharts_scatter.iteminfo' ),
-
-
-	# RELATIONSHIPS : SANKEY version
-	(r'^relationships/go$', 'labsapp.relationships.index' ),
-	(r'^relationships/ajax/iteminfo', 'labsapp.relationships.iteminfo' ),
-	
-
-	# WITNESSES
-	(r'^witnesses/go$', 'labsapp.witnessesRgraph.index' ),
-	(r'^witnesses/go-halfsize$', 'labsapp.witnessesRgraph.index' , {'template': 'halfsize'}),
-	(r'^witnesses/ajax/iteminfo', 'labsapp.witnessesRgraph.iteminfo' ),
-
-
-
-	# --------
-	# INTERNAL URLS - testing and backup
-	# --------
-
-
-	# PERSON 2 PERSON  [dismissed]
-	(r'^person2person/go$', 'labsapp.person2person.index' ),
-	(r'^person2person/go-halfsize$', 'labsapp.person2person.index' , {'template': 'halfsize'}),
-	# (r'^person2person/ajax/iteminfo', 'labsapp.person2person.iteminfo' ),
-				
-	(r'^bubbles$', 'labsapp.bubbles.index' ),
-	
-	# highcharts	
-	(r'^scatter$', 'labsapp.highcharts_scatter.scatter' ),
-	(r'^scatter_example$', 'labsapp.highcharts_scatter.scatter_example' ),
-	(r'^scatterpeople$', 'labsapp.highcharts_scatter.scatter_peopleage' ),
-	
-	
-	# still in the making..
-	
-	(r'^tree$', 'labsapp.tree.tree1' ),
-
-
-)
-
+]

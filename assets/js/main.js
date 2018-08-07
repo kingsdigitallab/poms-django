df71b03121fab524dc4b827d36143523887e1d1f
Elliott Hall <elliott.hall@kcl.ac.uk>
2018-08-07 18:15:19 +0100

    toggle buttons for facet groups

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,42 +13,44 @@ $(function () {
         e.preventDefault();
         var facet_group = $(this).data('facet_group');
         var facet_name = $(this).data('facet_name');
-        var selected_facets = $(this).data('selected_facets');
-        var title = this;
-        var url = "/browse/" + facet_group + "/" + facet_name + "/?";
-        if (selected_facets && selected_facets.length > 0) {
-            url += selected_facets;
-        }
-        if ($('div.' + facet_group + '__' + facet_name).html().length < 1) {
-            $.get(url, function (data) {
-                var div = $('div.' + facet_group + '__' + facet_name);
-                if (div.length > 0) {
-                    var content = div[0];
-                    $(content).append(data);
-                }
-                $("#" + facet_name + "_facetfilter").on("keyup", filterFacets);
+        var label=$(this).children('strong');
+        if ($(this).attr('class').indexOf('loaded') < 0) {
+            // Load the facet choices
+            var selected_facets = $(this).data('selected_facets');
+            var title = this;
+            var url = "/browse/" + facet_group + "/" + facet_name + "/?";
+            if (selected_facets && selected_facets.length > 0) {
+                url += selected_facets;
+            }
+            if ($('div.' + facet_group + '__' + facet_name).html().length < 1) {
+                $.get(url, function (data) {
+                    var div = $('div.' + facet_group + '__' + facet_name);
+                    if (div.length > 0) {
+                        var content = div[0];
+                        $(content).append(data);
+                    }
+                    $("#" + facet_name + "_facetfilter").on("keyup", filterFacets);
+                    $(label).html($(label).html().replace('+','-'));
+                });
+            }
 
-            });
+            $(this).toggleClass('loaded');
+            $(this).toggleClass('active');
+        } else{
+            if ($(this).attr('class').indexOf('active') > 0){
+                // Already loaded and open
+                $(label).html($(label).html().replace('-','+'));
+            } else{
+                //Loaded and closed
+                $(label).html($(label).html().replace('+','-'));
+            }
+            $('div.' + facet_group + '__' + facet_name).slideToggle(400);
+            $(this).toggleClass('active');
         }
-        /*
-        if ($(title).data("load") == 1 &&factoidTypeKey > 0 && personid > 0) {
-            $.get("/factoidgroup/" + personid + "/" + factoidTypeKey + "/", function (data) {
-                var ul = $(title).next().children('ul');
-                if (ul.length > 0) {
-                    var content = ul[0];
-                    $(content).html(data);
-                    $(title).data("load",0);
-                }
-            });
-        }*/
+
     });
 
-    // Expand / Collapse
-    $('.facet-group-name').bind('click', function () {
-        $(this).next('.facets-list').slideToggle(400).toggleClass('hide');
-        $(this).toggleClass('active');
-        return false;
-    });
+
 
     //new gRanger('#range', '#min', '#max');
 });

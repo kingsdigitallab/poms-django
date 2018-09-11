7fb9c4f653c0d00f8141e7683d0d84b740652b0b
Brian Maher <brian.maher@kcl.ac.uk>
2018-09-11 09:44:15 +0000

    Added wagtail hook to whitelist HTML elements in RTE

--- pomsapp_wagtail/wagtail_hooks.py
+++ pomsapp_wagtail/wagtail_hooks.py
@@ -1,6 +1,33 @@
 from django.conf import settings
 from django.utils.html import format_html, format_html_join
 from wagtail.wagtailcore import hooks
+from wagtail.wagtailcore.whitelist import attribute_rule, check_url
+
+
+def whitelister_element_rules():
+    return {
+        'p': attribute_rule({'class': True}),
+        'a': attribute_rule({'href': check_url, 'id': True, 'class': True,
+                             'target': True}),
+        'span': attribute_rule({'class': True}),
+        'i': attribute_rule({'class': True}),
+        'iframe': attribute_rule(
+            {'id': True, 'class': True, 'src': True, 'style': True,
+             'frameborder': True, 'allowfullscreen': True, 'width': True,
+             'height': True}),
+        'small': attribute_rule({'class': True}),
+        'table': attribute_rule({'class': True, 'id': True, 'style': True}),
+        'tr': attribute_rule({'class': True, 'id': True, 'style': True}),
+        'td': attribute_rule({'class': True, 'id': True, 'style': True}),
+        'th': attribute_rule({'class': True, 'id': True, 'style': True}),
+        'b': attribute_rule({'class': True}),
+        'strong': attribute_rule({'class': True})
+
+    }
+
+
+hooks.register('construct_whitelister_element_rules',
+               whitelister_element_rules)
 
 
 def editor_js():

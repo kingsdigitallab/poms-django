8e6df9ff601d67014bc781d544dc7e632e247266
Brian Maher <brian.maher@kcl.ac.uk>
2018-09-12 10:31:02 +0000

    Template issues

--- pomsapp/urls_browse.py
+++ pomsapp/urls_browse.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from pomsapp.views import PomsFacetedBrowse
 
 urlpatterns = [
-    url(r'^$', PomsFacetedBrowse.as_view(), name='browse'),
+    url(r'^$', PomsFacetedBrowse.as_view(), name='search'),
     url(r'(?P<facet_group>\w+)/(?P<facet_name>\w+)/$', PomsFacetedBrowse.as_view(
         template_name='pomsapp/browse/facet_choices.html',
         ajax=True,

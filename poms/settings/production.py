aa6c0893973791340a29eff4c4f7d82f12e2e562
Elliott Hall <elliott.hall@kcl.ac.uk>
2024-03-26 17:40:55 +0000

    requirements update

--- poms/settings/production.py
+++ poms/settings/production.py
@@ -6,13 +6,12 @@ from .base import *  # noqa
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=['poms-os.kdl.kcl.ac.uk'])
-
 INTERNAL_IPS = ['0.0.0.0', '127.0.0.1', '::1', '10.0.2.2']
 
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-#DATABASES = {"default": env.db("DATABASE_URL")}
+
 
 DATABASES = {
     'default': {
